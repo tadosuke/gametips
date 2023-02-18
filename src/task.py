1bf45a987368e3bbe5686d17471794b594d81f86
tadosuke <tadosuke@gmail.com>
2023-02-18 16:46:32 +0900

    削除機能を追加

--- src/task.py
+++ src/task.py
@@ -10,6 +10,7 @@ class TaskBase:
 	
 	def __init__(self, parent: TaskBase = None) -> None:
 		self._children: list[TaskBase] = []
+		self._exit = False
 
 		if parent is not None:
 			parent.add_child(self)
@@ -25,18 +26,43 @@ class TaskBase:
 		:param 前回更新からの経過時間（秒）
 		"""
 
-		self._update(delta_sec)
+		if self._exit:
+			return
 
+		# 自分の更新
+		self._update(delta_sec)
+		if self._exit:
+			return
+
+		# 子の更新
 		for child in self._children:
 			child.update(delta_sec)
+
+		# 終了している子を削除する
+		self._apply_exit_children()
 
 	def _update(self, delta_sec: float) -> None:
 		"""自分の更新."""
 
 		pass
 
+	def _apply_exit_children(self):
+		"""終了している子を削除する."""
+
+		self._children = [c for c in self._children if not c.is_exit()]
+
 	def add_child(self, child: TaskBase) -> None:
 		"""子タスクを追加する."""
 
 		assert child not in self._children
 		self._children.append(child)
+
+	def exit(self) -> None:
+		"""タスクを終了する."""
+
+		self._exit = True
+
+	def is_exit(self) -> bool:
+		"""終了しているか."""
+
+		return self._exit
--- tests/test_task.py
+++ tests/test_task.py
@@ -1,6 +1,8 @@
 """タスクシステムモジュールのテスト."""
 
 import unittest
+from unittest import mock
+
 from task import TaskBase
 
 
@@ -41,6 +43,7 @@ class TestTask(unittest.TestCase):
 	def test_init(self):
 		a = TaskA()
 		self.assertFalse(a.has_child())
+		self.assertFalse(a.is_exit())
 
 	def test_add_child(self):
 		a = TaskA()
@@ -66,6 +69,36 @@ class TestTask(unittest.TestCase):
 		d = TaskD(b)
 		a.update(0)
 		self.assertEqual('abdc', _order)
+
+	def test_exit(self):
+		a = TaskA()
+
+		with mock.patch.object(a, '_update') as mp_update:
+			a.update(0)
+			mp_update.assert_called_once_with(0)
+
+		a.exit()
+		self.assertTrue(a.is_exit())
+
+		# 終了後の update は実行されない
+		with mock.patch.object(a, '_update') as mp_update:
+			a.update(0)
+			mp_update.assert_not_called()
+
+	def test_apply_exit_children(self):
+		a = TaskA()
+		b = TaskB(a)
+		c = TaskC(a)
+
+		# 終了している b を削除。c がまだ残っている
+		b.exit()
+		a._apply_exit_children()
+		self.assertTrue(a.has_child())
+
+		# c も終了したので削除。子タスクはいなくなる
+		c.exit()
+		a._apply_exit_children()
+		self.assertFalse(a.has_child())
 
 
 if __name__ == '__main__':
