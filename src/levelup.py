e38131cdae3cca1c217bc3b05e120307f519320a
tadosuke <tadosuke@gmail.com>
2023-02-23 22:38:28 +0900

    コメントを修正

--- src/levelup.py
+++ src/levelup.py
@@ -4,101 +4,94 @@ import typing as tp
 
 
 class Calculator:
-	"""レベルアップ計算機.
+    """レベルアップ計算機.
 
-	:param exps: レベルごとの経験値
-	"""
+    :param exps: レベルごとの経験値
+    """
 
-	def __init__(
-			self,
-			exps: tp.Sequence[int]) -> None:
-		assert exps
-		self._exps = tuple(exps)
+    def __init__(
+            self,
+            exps: tp.Sequence[int]) -> None:
+        assert exps
+        self._exps = tuple(exps)
 
-	def calc(self, exp: int) -> int:
-		"""経験値に対応するレベルを計算します.
+    def calc(self, exp: int) -> int:
+        """経験値に対応するレベルを計算します.
 
-		:param exp: 経験値
-		:return: レベル
-		"""
+        :param exp: 経験値
+        :return: レベル
+        """
+        assert 0 <= exp
 
-		assert 0 <= exp
+        for lv, e in enumerate(self._exps):
+            if exp < e:
+                return lv
+        return self.get_max_level()
 
-		for lv, e in enumerate(self._exps):
-			if exp < e:
-				return lv
-		return self.get_max_level()
+    def get_max_level(self) -> int:
+        """最大レベルを得ます.
 
-	def get_max_level(self) -> int:
-		"""最大レベルを得ます.
-
-		:return: 最大レベル
-		"""
-
-		return len(self._exps)
+        :return: 最大レベル
+        """
+        return len(self._exps)
 
 
 class Character:
-	"""キャラクター.
+    """キャラクター.
 
-	:param calculator: レベル計算オブジェクト
-	:param exp: 初期経験値
-	"""
+    :param calculator: レベル計算オブジェクト
+    :param exp: 初期経験値
+    """
 
-	def __init__(
-			self,
-			calculator: Calculator,
-			exp: int = 0,
-			name: str = '') -> None:
-		assert 0 <= exp
+    def __init__(
+            self,
+            calculator: Calculator,
+            exp: int = 0,
+            name: str = '') -> None:
+        assert 0 <= exp
 
-		self._calculator = calculator
-		self._name = name
-		self._exp = exp
-		self._level = 0
+        self._calculator = calculator
+        self._name = name
+        self._exp = exp
+        self._level = 0
 
-		self._update_level()
+        self._update_level()
 
-	@property
-	def exp(self) -> int:
-		"""経験値."""
+    @property
+    def exp(self) -> int:
+        """経験値."""
+        return self._exp
 
-		return self._exp
+    @property
+    def name(self) -> str:
+        """名前."""
+        return self._name
 
-	@property
-	def name(self) -> str:
-		"""名前."""
+    @property
+    def level(self) -> int:
+        """レベル."""
+        return self._level
 
-		return self._name
+    def add_exp(self, exp: int) -> int:
+        """経験値を加算します.
 
-	@property
-	def level(self) -> int:
-		"""レベル."""
+        :param exp: 経験値
+        :return: レベルの上昇量
+        """
+        assert 0 <= exp
+        self._exp += exp
+        diff = self._update_level()
 
-		return self._level
+        return diff
 
-	def add_exp(self, exp: int) -> int:
-		"""経験値を加算します.
+    def _update_level(self) -> int:
+        """レベルを計算し直す.
 
-		:param exp: 経験値
-		:return: レベルの上昇量
-		"""
+        :return レベルの上昇量
+        """
+        before = self._level
+        self._level = self._calculator.calc(self._exp)
+        return self.level - before
 
-		assert 0 <= exp
-		self._exp += exp
-		diff = self._update_level()
-
-		return diff
-
-	def _update_level(self) -> int:
-		"""レベルを計算し直す.
-
-		:return レベルの上昇量
-		"""
-
-		before = self._level
-		self._level = self._calculator.calc(self._exp)
-		return self.level - before
-
-	def __str__(self) -> str:
-		return f'{self.name}: LV={self.level}, EXP={self.exp}'
+    def __str__(self) -> str:
+        return f'{self.name}: LV={self.level}, EXP={self.exp}'
