c2a95eb8b296ce770368d6d0ae1f4f34af9ffd66
tadosuke <tadosuke@gmail.com>
2023-02-26 08:07:38 +0900

    ステータス計算

---
+++ src/battlestatus.py
@@ -0,0 +1,145 @@
+from enum import Flag, Enum, auto
+
+
+class ConditionId(Flag):
+    ATK_UP = auto()  # 攻撃力アップ
+    ATK_DOWN = auto()  # 攻撃力ダウン
+
+
+class Condition:
+
+    def __init__(self, id_=None):
+        self._id = id_
+
+    def has(self, id_):
+        if self._id is None:
+            return False
+        return id_ in self._id
+
+    def add(self, id_):
+        if self._id is None:
+            self._id = id_
+        else:
+            self._id |= id_
+        self._offset()
+
+    def remove(self, id_):
+        if self._id is None:
+            return
+        self._id &= ~id_
+        self._offset()
+
+    def _offset(self):
+        both = ConditionId.ATK_UP | ConditionId.ATK_DOWN
+        if both in self._id:
+            self._id = None
+
+
+class SkillId(Enum):
+    ATK_UP = auto()  # 攻撃力アップ
+    DEF_UP = auto()  # 防御力アップ
+
+
+class SkillDict:
+
+    def __init__(self):
+        self._dict: dict[SkillId, int] = {}
+
+    def add(self, skill_id, level=1):
+        self._dict[skill_id] = level
+
+    def has(self, skill_id):
+        return skill_id in self._dict
+
+    def get_level(self, skill_id):
+        if not self.has(skill_id):
+            return 0
+        return self._dict[skill_id]
+
+    def apply_atk(self, atk):
+        if self.has(SkillId.ATK_UP):
+            atk += self.get_level(SkillId.ATK_UP) * 2
+        return atk
+
+
+class Skill:
+
+    def __init__(self, id_, level=1):
+        assert 1 <= level
+
+        self._id = id_
+        self._level = level
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def level(self):
+        return self._level
+
+    def set_level(self, level):
+        assert 1 <= level
+
+        self._level = level
+
+    def apply_atk(self, atk):
+        if self.id == SkillId.ATK_UP:
+            atk += self.level * 2
+        return atk
+
+
+class Weapon:
+
+    def __init__(self, atk):
+        self._atk = atk
+
+    @property
+    def atk(self):
+        return self._atk
+
+
+class Character:
+
+    def __init__(self, atk=10):
+        self._atk = atk
+        self._weapon = None
+        self._condition = Condition()
+        self._skills = SkillDict()
+
+    @property
+    def condition(self):
+        return self._condition
+
+    def equip(self, weapon):
+        self._weapon = weapon
+
+    def unequip(self):
+        self._weapon = None
+
+    @property
+    def weapon(self):
+        return self._weapon
+
+    @property
+    def skills(self):
+        return self._skills
+
+    def calc_atk(self):
+        atk = self._atk
+        atk = self._calc_atk_weapon(atk)
+        atk = self._calc_atk_condition(atk)
+        atk = self._skills.apply_atk(atk)
+        return int(atk)
+
+    def _calc_atk_weapon(self, atk):
+        if self.weapon is not None:
+            atk += self.weapon.atk
+        return atk
+
+    def _calc_atk_condition(self, atk):
+        if self.condition.has(ConditionId.ATK_UP):
+            atk *= 1.25
+        elif self.condition.has(ConditionId.ATK_DOWN):
+            atk *= 0.75
+        return atk
---
+++ tests/test_battlestatus.py
@@ -0,0 +1,153 @@
+import unittest
+from unittest import mock
+
+from battlestatus import Character, Condition, ConditionId, Weapon, SkillId, Skill, SkillDict
+
+
+class TestCondition(unittest.TestCase):
+
+    def test_init(self):
+        cond = Condition()
+        self.assertIsNone(cond._id)
+
+    def test_add_remove(self):
+        cond = Condition()
+        cond.add(ConditionId.ATK_UP)
+        self.assertTrue(
+            cond.has(ConditionId.ATK_UP))
+
+        cond.remove(ConditionId.ATK_UP)
+        self.assertFalse(
+            cond.has(ConditionId.ATK_UP))
+
+    def test_condition_offset(self):
+        cond = Condition()
+        cond.add(ConditionId.ATK_UP)
+        cond.add(ConditionId.ATK_DOWN)
+        self.assertFalse(
+            cond.has(ConditionId.ATK_UP))
+        self.assertFalse(
+            cond.has(ConditionId.ATK_DOWN))
+
+
+class TestWeapon(unittest.TestCase):
+
+    def test_init(self):
+        w = Weapon(4)
+        self.assertEqual(4, w.atk)
+
+
+class TestSkillDict(unittest.TestCase):
+
+    def test_init(self):
+        sd = SkillDict()
+        self.assertEqual({}, sd._dict)
+
+    def test_add(self):
+        sd = SkillDict()
+        self.assertFalse(sd.has(SkillId.ATK_UP))
+        sd.add(SkillId.ATK_UP)
+        self.assertTrue(sd.has(SkillId.ATK_UP))
+
+    def test_get_level(self):
+        sd = SkillDict()
+        self.assertEqual(0, sd.get_level(SkillId.ATK_UP))
+        sd.add(SkillId.ATK_UP, 2)
+        self.assertEqual(2, sd.get_level(SkillId.ATK_UP))
+
+    def test_apply_atk(self):
+        sd = SkillDict()
+        self.assertEqual(10, sd.apply_atk(10))
+        sd.add(SkillId.DEF_UP)
+        self.assertEqual(10, sd.apply_atk(10))
+        sd.add(SkillId.ATK_UP)
+        self.assertEqual(12, sd.apply_atk(10))
+        sd.add(SkillId.ATK_UP, 3)
+        self.assertEqual(16, sd.apply_atk(10))
+
+
+class TestSkill(unittest.TestCase):
+
+    def test_init(self):
+        s = Skill(SkillId.ATK_UP)
+        self.assertEqual(SkillId.ATK_UP, s.id)
+        self.assertEqual(1, s.level)
+
+    def test_level(self):
+        s = Skill(SkillId.ATK_UP)
+
+        s.set_level(2)
+        self.assertEqual(2, s.level)
+
+        with self.assertRaises(AssertionError):
+            s.set_level(-1)
+
+    def test_apply_atk(self):
+        s = Skill(SkillId.ATK_UP)
+        atk = s.apply_atk(10)
+        self.assertEqual(12, atk)
+
+        s = Skill(SkillId.DEF_UP)
+        atk = s.apply_atk(10)
+        self.assertEqual(10, atk)
+
+
+class TestCharacter(unittest.TestCase):
+
+    def test_init(self):
+        c = Character()
+        self.assertEqual(10, c._atk)
+        self.assertIsInstance(c.condition, Condition)
+        self.assertIsNotNone(c.skills)
+
+    def test_equip(self):
+        c = Character()
+
+        w = Weapon(4)
+        c.equip(w)
+        self.assertIs(w, c.weapon)
+
+        c.unequip()
+        self.assertIsNone(c.weapon)
+
+    def test_calc_atk(self):
+        c = Character(10)
+        self.assertEqual(10, c.calc_atk())
+
+        c = Character(10)
+        c.skills.add(SkillId.ATK_UP)
+        self.assertEqual(12, c.calc_atk())
+
+        c = Character(10)
+        c.condition.add(ConditionId.ATK_UP)
+        self.assertEqual(12, c.calc_atk())
+
+    def test_calc_atk_weapon(self):
+        c = Character()
+        atk = c._calc_atk_weapon(5)
+        self.assertEqual(5, atk)
+
+        c = Character()
+        w = Weapon(4)
+        c.equip(w)
+        atk = c._calc_atk_weapon(5)
+        self.assertEqual(9, atk)
+
+    def test_calc_atk_condition(self):
+        c = Character()
+        atk = c._calc_atk_condition(10)
+        self.assertEqual(10, atk)
+
+        c = Character()
+        c.condition.add(ConditionId.ATK_UP)
+        atk = c._calc_atk_condition(10)
+        self.assertAlmostEqual(12.5, atk)
+
+        c = Character()
+        c.condition.add(ConditionId.ATK_DOWN)
+        atk = c._calc_atk_condition(10)
+        self.assertAlmostEqual(7.5, atk)
+
+
+if __name__ == '__main__':
+    unittest.main()
