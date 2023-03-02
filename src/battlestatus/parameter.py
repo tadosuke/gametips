93bd7ec23822f9784b2214aafc4003eea1049fbc
tadosuke <tadosuke@gmail.com>
2023-03-03 06:57:11 +0900

    パラメーター定義を追加

--- src/battlestatus/parameter.py
+++ src/battlestatus/parameter.py
@@ -1,4 +1,67 @@
 """戦闘パラメーター."""
+from __future__ import annotations
+
+
+class ParameterValue:
+    """パラメーターの値.
+
+    :param value: 初期値
+    """
+
+    def __init__(self, value: int) -> None:
+        if value < 0:
+            raise ValueError
+
+        self._value = value
+
+    @property
+    def value(self) -> int:
+        return self._value
+
 
 class Parameter:
-    pass
+    """戦闘パラメーター."""
+
+    def __init__(self):
+        self._atk = ParameterValue(0)
+        self._def = ParameterValue(0)
+        self._mat = ParameterValue(0)
+        self._mdf = ParameterValue(0)
+        self._dex = ParameterValue(0)
+        self._spd = ParameterValue(0)
+        self._luk = ParameterValue(0)
+
+    @property
+    def attack(self) -> ParameterValue:
+        """攻撃力."""
+        return self._atk
+
+    @property
+    def defence(self) -> ParameterValue:
+        """防御力."""
+        return self._def
+
+    @property
+    def mattack(self) -> ParameterValue:
+        """魔法攻撃力."""
+        return self._mat
+
+    @property
+    def mdefence(self) -> ParameterValue:
+        """魔法防御力."""
+        return self._mdf
+
+    @property
+    def dexterity(self) -> ParameterValue:
+        """器用さ."""
+        return self._dex
+
+    @property
+    def speed(self) -> ParameterValue:
+        """素早さ."""
+        return self._spd
+
+    @property
+    def luck(self) -> ParameterValue:
+        """運."""
+        return self._luk
--- tests/battlestatus/test_parameter.py
+++ tests/battlestatus/test_parameter.py
@@ -3,13 +3,30 @@
 import unittest
 from enum import Enum, auto
 
-from battlestatus.parameter import Parameter
+from battlestatus.parameter import Parameter, ParameterValue
+
+
+class TestParameterValue(unittest.TestCase):
+
+    def test_init(self):
+        value = ParameterValue(10)
+        self.assertEqual(10, value.value)
+
+        with self.assertRaises(ValueError):
+            ParameterValue(-10)
 
 
 class TestParameter(unittest.TestCase):
 
     def test_init(self):
-        pass
+        param = Parameter()
+        self.assertEqual(0, param.attack.value)
+        self.assertEqual(0, param.defence.value)
+        self.assertEqual(0, param.mattack.value)
+        self.assertEqual(0, param.mdefence.value)
+        self.assertEqual(0, param.dexterity.value)
+        self.assertEqual(0, param.speed.value)
+        self.assertEqual(0, param.luck.value)
 
 
 if __name__ == '__main__':
